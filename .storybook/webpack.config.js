7414ab003ff6820e985b627ec7842ac67b6ae7cf
Jojo Tutor <josaphat@dnamicro.com>
2019-10-03 16:27:03 +0800

    sass loader

--- .storybook/webpack.config.js
+++ .storybook/webpack.config.js
@@ -1,4 +1,18 @@
+const path = require("path");
+
 module.exports = ({ config }) => {
+  // scss loader
+  config.module.rules.push({
+    test: /\.scss$/,
+    loaders: ["style-loader", "css-loader", "sass-loader"],
+    include: path.resolve(__dirname, "../src"),
+    exclude: [
+      path.resolve(__dirname, "../lib"),
+      path.resolve(__dirname, "../node_modules"),
+    ],
+  });
+
+  // story source
   config.module.rules.push({
     test: /\.stories\.jsx?$/,
     loaders: [require.resolve("@storybook/addon-storysource/loader")],
