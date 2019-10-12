eb338228f2cc2402eee55079abf04816995f3806
Jojo Tutor <jeshrave003@gmail.com>
2019-10-12 18:23:54 +0800

    field set

--- src/forms/fieldset.stories.js
+++ src/forms/fieldset.stories.js
@@ -10,99 +10,103 @@ export default {
 };
 
 export const all = () => (
-  <Grid container spacing={2} className="storybook-adjusted">
-    {/* Fieldset */}
-    <Grid item xs={12}>
-      <Text component="h1" className="form-title">Fieldset</Text>
-    </Grid>
-
-    {/* Personal Information */}
-    <Grid item xs={12}>
-      <Text component="h2" className="form-sub-title">Personal Information</Text>
-    </Grid>
-
-    {/* First name, Last name */}
+  <Grid className="storybook-adjusted" container justify="center">
     <Grid item xs={6}>
-      <TextField
-        fullWidth
-        id="firstName"
-        label="First name"
-        defaultValue="Jane"
-      />
-    </Grid>
-    <Grid item xs={6}>
-      <TextField
-        fullWidth
-        id="lastName"
-        label="Last name"
-        defaultValue="Custodio"
-      />
-    </Grid>
+      <Grid container spacing={2}>
+        {/* Fieldset */}
+        <Grid item xs={12}>
+          <Text component="h1" className="form-title">Fieldset</Text>
+        </Grid>
 
-    {/* Email Address, Contact Number */}
-    <Grid item xs={6}>
-      <TextField
-        fullWidth
-        id="email"
-        label="Email Address"
-        defaultValue="jcustodio@email.com"
-      />
-    </Grid>
-    <Grid item xs={6}>
-      <TextField
-        fullWidth
-        id="contactNumber"
-        label="Contact Number"
-        defaultValue="(064)-6294-275"
-      />
-    </Grid>
+        {/* Personal Information */}
+        <Grid item xs={12}>
+          <Text component="h2" className="form-sub-title">Personal Information</Text>
+        </Grid>
 
-    {/* Personal Information */}
-    <Grid item xs={12}>
-      <Text component="h2" className="form-sub-title">Address Details</Text>
-    </Grid>
+        {/* First name, Last name */}
+        <Grid item xs={6}>
+          <TextField
+            fullWidth
+            id="firstName"
+            label="First name"
+            defaultValue="Jane"
+          />
+        </Grid>
+        <Grid item xs={6}>
+          <TextField
+            fullWidth
+            id="lastName"
+            label="Last name"
+            defaultValue="Custodio"
+          />
+        </Grid>
 
-    {/* Address 1 */}
-    <Grid item xs={12}>
-      <TextField
-        fullWidth
-        id="address1"
-        label="Address 1"
-      />
-    </Grid>
+        {/* Email Address, Contact Number */}
+        <Grid item xs={6}>
+          <TextField
+            fullWidth
+            id="email"
+            label="Email Address"
+            defaultValue="jcustodio@email.com"
+          />
+        </Grid>
+        <Grid item xs={6}>
+          <TextField
+            fullWidth
+            id="contactNumber"
+            label="Contact Number"
+            defaultValue="(064)-6294-275"
+          />
+        </Grid>
 
-    {/* Address 2 */}
-    <Grid item xs={12}>
-      <TextField
-        fullWidth
-        id="address2"
-        label="Address 2"
-      />
-    </Grid>
+        {/* Personal Information */}
+        <Grid item xs={12}>
+          <Text component="h2" className="form-sub-title">Address Details</Text>
+        </Grid>
 
-    {/* Town / City */}
-    <Grid item xs={6}>
-      <Select
-        fullWidth
-        label="Town / City"
-        value={0}
-        onChange={() => {}}
-      >
-        <MenuItem value={0}>Select one</MenuItem>
-        <MenuItem value={1}>City 1</MenuItem>
-        <MenuItem value={2}>City 2</MenuItem>
-        <MenuItem value={3}>City 3</MenuItem>
-        <MenuItem value={4}>City 4</MenuItem>
-      </Select>
-    </Grid>
+        {/* Address 1 */}
+        <Grid item xs={12}>
+          <TextField
+            fullWidth
+            id="address1"
+            label="Address 1"
+          />
+        </Grid>
 
-    {/* ZIP Code */}
-    <Grid item xs={6}>
-      <TextField
-        fullWidth
-        id="zipCode"
-        label="ZIP Code"
-      />
+        {/* Address 2 */}
+        <Grid item xs={12}>
+          <TextField
+            fullWidth
+            id="address2"
+            label="Address 2"
+          />
+        </Grid>
+
+        {/* Town / City */}
+        <Grid item xs={6}>
+          <Select
+            fullWidth
+            label="Town / City"
+            value={0}
+            onChange={() => {}}
+          >
+            <MenuItem value={0}>Select one</MenuItem>
+            <MenuItem value={1}>City 1</MenuItem>
+            <MenuItem value={2}>City 2</MenuItem>
+            <MenuItem value={3}>City 3</MenuItem>
+            <MenuItem value={4}>City 4</MenuItem>
+          </Select>
+        </Grid>
+
+        {/* ZIP Code */}
+        <Grid item xs={6}>
+          <TextField
+            fullWidth
+            id="zipCode"
+            label="ZIP Code"
+          />
+        </Grid>
+      </Grid>
     </Grid>
   </Grid>
 );
