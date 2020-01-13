56f1e2b0b7df1d9073e99746061b138c5203b6e2
Josaphat Tutor <jtutor@kgsbuildings.com>
2020-01-13 22:41:43 +0800

    added controls

---
+++ src/components/container/container.js
@@ -0,0 +1,44 @@
+import React from "react";
+import PropTypes from "prop-types";
+import clsx from "clsx";
+import styled from "styled-components";
+import { Container } from "@material-ui/core";
+import { withTheme } from "dry/theme";
+import setPropTypes from "dry/utils/setPropTypes";
+
+function DryContainer(props) {
+  const {
+    className,
+    ...restProps
+  } = props;
+  const clsxName = clsx(className, {
+
+  });
+
+  return (
+    <Container
+      {...restProps}
+      className={clsxName}
+    />
+  );
+}
+
+DryContainer.defaultProps = {
+  className: "",
+
+};
+
+DryContainer.propTypes = {
+  className: PropTypes.string,
+
+};
+
+const StyledContainer = styled(DryContainer)`
+ 
+`;
+
+const ThemedStyledContainer = withTheme(StyledContainer);
+
+setPropTypes("DryContainer", Container, ThemedStyledContainer);
+
+export default ThemedStyledContainer;
