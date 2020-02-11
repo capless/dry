fd0ee86fbe73f8ddd481fa8acc401f45d21b9d56
Josaphat Tutor <jtutor@kgsbuildings.com>
2020-02-12 06:05:14 +0800

    dialog components

---
+++ src/components/dialog-content-text/dialog-content-text.js
@@ -0,0 +1,44 @@
+import React from "react";
+import PropTypes from "prop-types";
+import clsx from "clsx";
+import styled from "styled-components";
+import { DialogContentText } from "@material-ui/core";
+import { withTheme } from "dry/theme";
+import setPropTypes from "dry/utils/setPropTypes";
+
+function DryDialogContentText(props) {
+  const {
+    className,
+    ...restProps
+  } = props;
+  const clsxName = clsx(className, {
+
+  });
+
+  return (
+    <DialogContentText
+      {...restProps}
+      className={clsxName}
+    />
+  );
+}
+
+DryDialogContentText.defaultProps = {
+  className: "",
+
+};
+
+DryDialogContentText.propTypes = {
+  className: PropTypes.string,
+
+};
+
+const StyledDialogContentText = styled(DryDialogContentText)`
+ 
+`;
+
+const ThemedStyledDialogContentText = withTheme(StyledDialogContentText);
+
+setPropTypes("DryDialogContentText", DialogContentText, ThemedStyledDialogContentText);
+
+export default ThemedStyledDialogContentText;
